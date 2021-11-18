1868b2e4376dbbef318553c46004bae808f9b65c
Amelia Davis <ameliaaltman22@gmail.com>
2021-11-18 16:34:34 -0700

    resolved issue with event.js

--- Server/Models/Event.js
+++ Server/Models/Event.js
@@ -1,20 +1,12 @@
-const router = require("express"). Router();
-const Event = require("./Event");
-const moment = require("moment");
-    
-    router.post("/create-event", async (req, res ) => {
-        const event = Event(req.body);
-        await event.save();
-        res.sendStatus(201)
-    })
+const mongoose = require('mongoose');
 
-    router.get("/get-events", async (req, res) =>{
-        const events = await Event.find({
-            start:{$gte: moment(req.query.start).toDate() }, 
-            end:{$lte: moment(req.query.end).toDate() },
-        });
 
-        res.send(events);
-    })
+const EventSchema = mongoose.Schema({
+    start: Date, 
+    end: Date, 
+    title: String
+})
 
-module.exports = router; 
+const Event = mongoose.model("Event", EventSchema);
+
+module.exports = Event;
